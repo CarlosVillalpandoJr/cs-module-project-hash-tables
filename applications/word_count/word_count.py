11cb30c6806e4ea9c94ae45aa91100265732b942
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-06-03 16:47:17 -0500

    word count has two bugs

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,25 @@
+table = {}
+ignore = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
 def word_count(s):
-    # Your code here
+    new = []
+    for l in s:
+        letter = l.lower()
+        if letter in ignore:
+            letter = letter.replace(letter, '')
+        new.append(letter)
+    updated = ''.join(new).split(' ')
+    return tableHelper(updated)
+
+def tableHelper(list):
+    for word in list:
+        if word in table:
+            table[word] += 1
+        else:
+            table[word] = 1
+    return table
+
+
+        
 
 
 
@@ -7,4 +27,4 @@ if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    # print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
